e86c447dcb49e58ce4d8befb8349c100f3e2e953
CHidalgo405 <carlosignacio.9836@gmail.com>
2024-10-04 22:32:41 -0600

    feat: completar ListaDeTareas.java

--- ListaDeTareas.java
+++ ListaDeTareas.java
@@ -1,3 +1,34 @@
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class ListaDeTareas {
+    private List<Tarea> tareas;
+
+    public ListaDeTareas() {
+        tareas = new ArrayList<>();
+    }
+
+    public void agregarTarea(String descripcion) {
+        Tarea tarea = new Tarea(descripcion);
+        tareas.add(tarea);
+    }
+
+    public void eliminarTarea(int index) {
+        if (index >= 0 && index < tareas.size()) {
+            tareas.remove(index);
+        } else {
+            System.out.println("Índice de tarea inválido.");
+        }
+    }
+
+    public void mostrarTareas() {
+        if (tareas.isEmpty()) {
+            System.out.println("No hay tareas pendientes.");
+        } else {
+            for (int i = 0; i < tareas.size(); i++) {
+                System.out.println((i + 1) + ". " + tareas.get(i).getDescripcion());
+            }
+        }
+    }
+}
